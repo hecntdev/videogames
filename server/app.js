475d3cbb988aec1ec7ba8c7a3ca58a2c1880f105
Osvaldo Hernandez <joseo.hernandez@softtek.com>
2021-05-28 00:31:29 -0500

    esqueleto

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const uri = "mongodb://127.0.0.1:27017";
+const {MongoClient} = require('mongodb');
+
+const client = new MongoClient(uri);
+
+const init = async () => {
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+init()
